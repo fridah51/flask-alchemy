0233d04eb824c73356c3799295141342a390a2ba
fridah51 <61974299+fridah51@users.noreply.github.com>
2022-01-25 21:52:09 +0300

    Add files via upload

--- config/config.py
+++ config/config.py
@@ -11,11 +11,11 @@ class Development():
 
 class Production():
     SQLALCHEMY_DATABASE_URI='postgres://tpqyogvupfpiuh:a2eb22348ecb7bcd49217b088d170302e9b0bc82a27ece7d2baea0e16bc1f093@ec2-52-208-185-143.eu-west-1.compute.amazonaws.com:5432/d25bjau5n2s2rb'
-    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS")
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    DEBUG = os.getenv("DEBUG")
-    FLASK_APP = os.getenv("FLASK_APP")
-    FLASK_ENV = os.getenv("FLASK_ENV")
+    SECRET_KEY = '11ae8fcaceff9710e238b932e95072a1'
+    # SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS")
+    # DEBUG = os.getenv("DEBUG")
+    # FLASK_APP = os.getenv("FLASK_APP")
+    # FLASK_ENV = os.getenv("FLASK_ENV")
   
     
 # #production
