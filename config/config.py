162a5d267dd102ada1771832c2d574e9820db444
fridah51 <61974299+fridah51@users.noreply.github.com>
2022-01-25 17:44:39 +0300

    Add files via upload

--- config/config.py
+++ config/config.py
@@ -10,12 +10,18 @@ class Development():
     
 
 class Production():
-    SQLALCHEMY_DATABASE_URI='djweilvcuwseru://1ff6d594331d891c1b1296386d289248370ce544f138a77d288c9484715b2f38@ec2-18-203-64-130.eu-west-1.compute.amazonaws.com:5432/d795ll8t69t486'
-    SECRET_KEY = 'mygympartner'
-    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI").replace("://", "ql://", 1)
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS")
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    DEBUG = os.getenv("DEBUG")
+    FLASK_APP = os.getenv("FLASK_APP")
+    FLASK_ENV = os.getenv("FLASK_ENV")
+  
+    
 # #production
 # conn = psycopg2.connect(user='',
 #                         password='',
 #                         host='',
 #                         port='5432',
 #                         database='')
+#SQLALCHEMY_DATABASE_URI = postgresql://postgres:21post@localhost:5432/alchemy
