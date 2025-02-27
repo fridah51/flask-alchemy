99137fa7419baf3de38f3493e4af7304229c3172
fridah51 <fridahshiku1@gmail.com>
2025-02-27 06:34:50 +0300

    debug rebuild

--- config/config.py
+++ config/config.py
@@ -10,13 +10,12 @@ class Development():
     
 
 class Production():
-    # SQLALCHEMY_DATABASE_URI='postgresql://fdxcpgprlhsbwn:1eb19ae264d628d19e9bf2c9ffb7aeffb4e96d76a16810eaaa7309823bef4040@ec2-54-73-178-126.eu-west-1.compute.amazonaws.com:5432/d5bpd849bd40qp'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://alibabaa:12345@localhost:5432/alibabaa'
     SECRET_KEY = '11ae8fcaceff9710e238b932e95072a1'
     # SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS")
     # DEBUG = os.getenv("DEBUG")
     # FLASK_APP = os.getenv("FLASK_APP")
     # FLASK_ENV = os.getenv("FLASK_ENV")
-    SQLALCHEMY_DATABASE_URI = 'postgresql://alibabaa:12345@localhost:5432/alibabaa'
 
     
 # #production for paas-api
